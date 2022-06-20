b561c6731c08c713d03d105a09d1df50a894518a
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-06-20 11:43:05 -0400

    Ajustando table no banco p/ eloquent

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores'; //USAR SEMPRE QUE O NOME DA TABELA NÃO FOR SÓ ACRESCIDA DE S NO FINAL, POIS O ELOQUENT NÃO CONSIGUIRÁ ENCONTRA-LA SEM QUE EU SET ISSO
 }
