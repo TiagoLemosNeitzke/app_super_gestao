2a3743a42be1a3d2b0e21c5670d0b336b8a176c3
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2022-06-23 16:12:38 -0400

    persistindo dados no banco

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sitecontato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        var_dump($_POST);
+    public function contato(Request $request) {
+        /*echo '<pre>';
+        print_r($request->all());  O REQUEST RECEBE OS DADOS DO FORMULÁRIO COMO ARRAY | USAR $request->input('nomeDoCampo) 
+        echo '</pre>';
+        */
+        $contato = new Sitecontato();
+        $contato->create($request->all());
+
+        // AS DUAS MANEIRAS DE GRAVAR OS DADOS NO BANCO QUE ESTÃO ABAIXO PRECISAM DO MÉTODO SAVE()
+
+      //  $contato->fill($request->all()); | ESTÁ É OUTRA FORMA DE PERCISTIR OS DADOS NO BANCO
+
+        /* O CÓDIGO ABAIXO É UM EXEMPLO DE COMO POSSO POPULAR O BANCO COM OS DADOS VIDOS DO FRONT
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //print_r($contato->getAttributes());
+        $contato->save(); 
+        */
         return view('site.contato');
     }
 }
